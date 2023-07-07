772a40203e2e1ed24523a52035b9b75dea2d7e3d
nminy <minyoung9328@gmail.com>
2023-07-08 03:33:01 +0900

    Refactor: 아이콘 컴포넌트 생성

---
+++ src/components/Icons.jsx
@@ -0,0 +1,64 @@
+import {
+  FaWifi,
+  FaTv,
+  FaHotel,
+  FaWarehouse,
+  FaUmbrellaBeach,
+  FaHouseUser,
+  FaSwimmingPool,
+} from "react-icons/fa";
+import { BsHouseFill } from "react-icons/bs";
+import {
+  MdApartment,
+  MdForest,
+  MdCastle,
+  MdHouse,
+  MdHouseSiding,
+} from "react-icons/md";
+import { CgSmartHomeWashMachine } from "react-icons/cg";
+import {
+  GiHeatHaze,
+  GiCampingTent,
+  GiFamilyHouse,
+  GiMushroomHouse,
+  GiTreeSwing,
+} from "react-icons/gi";
+import {
+  RiHotelFill,
+  RiAncientGateFill,
+  RiBuilding4Fill,
+} from "react-icons/ri";
+import {
+  TbToolsKitchen2,
+  TbAirConditioning,
+  TbIroning2,
+  TbWashDry1,
+} from "react-icons/tb";
+export {
+  FaWifi,
+  FaTv,
+  FaHotel,
+  FaWarehouse,
+  FaUmbrellaBeach,
+  FaHouseUser,
+  FaSwimmingPool,
+  BsHouseFill,
+  MdApartment,
+  MdForest,
+  MdCastle,
+  MdHouse,
+  MdHouseSiding,
+  CgSmartHomeWashMachine,
+  GiHeatHaze,
+  GiCampingTent,
+  GiFamilyHouse,
+  GiMushroomHouse,
+  GiTreeSwing,
+  RiHotelFill,
+  RiAncientGateFill,
+  RiBuilding4Fill,
+  TbToolsKitchen2,
+  TbAirConditioning,
+  TbIroning2,
+  TbWashDry1,
+};
